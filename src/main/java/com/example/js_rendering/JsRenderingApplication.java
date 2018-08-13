d82bf32daad4b6aabed44ec554f2c27b130151e4
Brian Gibson <bggibson@hotmail.com>
2018-08-13 19:10:49 -0400

    Initial commit.

---
+++ src/main/java/com/example/js_rendering/JsRenderingApplication.java
@@ -0,0 +1,12 @@
+package com.example.js_rendering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsRenderingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsRenderingApplication.class, args);
+    }
+}
