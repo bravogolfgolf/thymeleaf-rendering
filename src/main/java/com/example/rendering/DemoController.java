3836321f40fa08753bf6643c7c7a2633acc99b34
Brian Gibson <bggibson@hotmail.com>
2018-08-14 07:11:01 -0400

    Refactors DemoController

--- src/main/java/com/example/rendering/DemoController.java
+++ src/main/java/com/example/rendering/DemoController.java
@@ -11,21 +11,18 @@ public class DemoController {
 
     @GetMapping(value = "/")
     public String index(Model model) {
-        System.out.println("HTML");
         model.addAttribute("hobbies", Arrays.asList("Cinema", "Poker", "Swimming"));
         return "index.html";
     }
 
     @GetMapping(value = "main.css")
     public String main(Model model) {
-        System.out.println("CSS");
         model.addAttribute("backgroundColor", "lightblue");
         return "main.css";
     }
 
     @GetMapping(value = "common.js")
     public String common(Model model) {
-        System.out.println("JAVASCRIPT");
         model.addAttribute("code", "Thymeleaf rules!".hashCode());
         return "common.js";
     }
